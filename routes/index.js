1c10cd4edb59f06fee153a44db1c83dbf16193b9
AleksandrukZakhar <zakharaleksandrukdev@gmail.com>
2022-10-29 10:51:34 +0300

    Add broken update

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const { urlencoded } = require("express");
 const express = require("express");
 const router = express.Router();
 const Game = require("../models/game.js");
@@ -11,11 +12,37 @@ router.get("/new", (req, res, next) => {
     res.render("form");
 });
 
-router.post("/new", (req, res, next) => {
+router.post("/new", async (req, res, next) => {
     const { title, price, free, imgUrl, genre } = req.body;
 
     const newGame = new Game({ title, price, free, img: imgUrl, genre });
     newGame.save((err, result) => (err ? next(err) : console.log(result)));
+
+    res.redirect("/");
+});
+
+router.get("/update/:id", (req, res, next) => {
+    res.render("form");
+});
+
+router.post("/update/:id", async (req, res, next) => {
+    const { title, price, free, imgUrl, genre } = req.body;
+
+    console.log(req.params.id);
+    await Game.updateOne(
+        { _id: req.params.id },
+        {
+            $set: {
+                title,
+                price,
+                free,
+                img: imgUrl,
+                genre,
+            },
+        },
+        { multi: true },
+        (err, result) => (err ? next(err) : console.log(result))
+    );
 
     res.redirect("/");
 });
